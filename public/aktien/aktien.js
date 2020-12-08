9ab81feda792ceabad373b946fcb2372dffcfab3
Benjamin Ader <benjamin.ader@porsche.de>
2020-12-08 15:42:23 +0100

    Aktien Page erweitert um Anforderungen, error handling

--- public/aktien/aktien.js
+++ public/aktien/aktien.js
@@ -52,9 +52,9 @@ for (var i = 0; i < rad.length; i++) {
   //Add an event listener to every radio option
   rad[i].addEventListener('change', function(change) {
 
-    //If radio selection changes, plot newly selected companies share history
-    let company = change.target.attributes.value.nodeValue;
-    plotShareHistory(company);
+  //If radio selection changes, plot newly selected companies share history
+  let company = change.target.attributes.value.nodeValue;
+  plotShareHistory(company);
 
   });
 
@@ -64,9 +64,10 @@ for (var i = 0; i < rad.length; i++) {
 }
 
 
+//Fetch share history and pass it to plotData function - if it fails, display an alert
 function plotShareHistory(company) {
 
-  //Fetch company information from 'alphavantage' API
+  //Fetch share history from 'alphavantage' API
   var requestOptions = {
   method: 'GET',
   redirect: 'follow'
@@ -82,14 +83,23 @@ function plotShareHistory(company) {
     .catch(error => {
 
       console.log(error);
-      alert("Die maximale Anzahl an alphavantage API calls wurde überschritten (5 pro Minute, 500 pro Monat). Bitte versuchen sie es später erneut.");
+
+      let alertContent = `
+        <div class="alert alert-dismissible alert-warning" style="display: block;">
+          <button type="button" class="close" data-dismiss="alert">&times;</button>
+          <h4 class="alert-heading">Action failed!</h4>
+          <p class="mb-0">This page uses the alphavantage API to fetch the data for displaying our share histories. As this API is free to use, it also comes with a limit of calls (max. 5 per minute, 500 per month). It seems like you've reached one of these limits - please try again!</p>
+        </div>
+      `;
+
+      document.getElementById('alert-box').innerHTML = alertContent;
 
     });
 
 }
 
-
-function plotData(company, data) {
+//Update the chartjs chart and stock information box in index.html
+async function plotData(company, data) {
 
   let timeSeries = data["Weekly Adjusted Time Series"];
 
@@ -105,17 +115,60 @@ function plotData(company, data) {
     data.push(flt);
   }
 
-  //Update chartjs chart
-  chart.data.labels.pop();
-    chart.data.datasets.forEach((dataset) => {
-        dataset.data.pop();
-    });
+  //Calculate max and min value over history
+  let max = Math.max.apply(null, data);
+  let min = Math.min.apply(null, data);
 
+  //Get most recent stock values
+  let recent = await getMostRecentValue(company);
+
+  console.log(recent);
+
+  //Parse HTML for info block
+  let newInfo = `
+    <table class="table table-hover" style="text-align: center;">
+      <tbody>
+        <tr class="table-primary">
+          <th scope="row">Current Value</th>
+          <td>${recent} USD</td>
+        </tr>
+        <tr class="table-primary">
+          <th scope="row">History MAX</th>
+          <td>${max} USD</td>
+        </tr>
+        <tr class="table-primary">
+          <th scope="row">History MIN</th>
+          <td>${min} USD</td>
+        </tr>
+      </tbody>
+    </table>
+  `;
+
+  //Update chartjs chart
   chart.data.labels = labels;
-    chart.data.datasets.forEach((dataset) => {
-        dataset.label = company;
-        dataset.data = data;
-    });
-    chart.update();
+  chart.data.datasets.forEach((dataset) => {
+      dataset.label = company;
+      dataset.data = data;
+  });
+
+  chart.update();
+  document.getElementById("companyData").innerHTML = newInfo;
+
+}
+
+//Fetch most recent stock value
+async function getMostRecentValue(company) {
+
+  //Fetch most recent stock value from 'alphavantage' API
+  var requestOptions = {
+    method: 'GET',
+    redirect: 'follow'
+  };
+
+  let response = await fetch("https://www.alphavantage.co/query?apikey=" + apiKey + "&function=TIME_SERIES_INTRADAY&interval=1min&symbol=" + company, requestOptions);
+  let json = await response.json();
+  let mostRecentValue = await Object.values(json["Time Series (1min)"])[0]["4. close"];
+
+  return mostRecentValue;
 
 }
